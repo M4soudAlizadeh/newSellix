8eb646f604a858939b3aa16e398b0e275b4f562f
M4soudalizadeh <M4soudalizadeh@gmail.com>
2023-06-14 23:14:12 +0330

    App responsive with tailwind is done

--- src/pages/MainPageComponent/AppSection.js
+++ src/pages/MainPageComponent/AppSection.js
@@ -2,15 +2,17 @@ import classes from "./AppSection.module.css";
 
 const AppSection = () => {
   return (
-    <div className={classes["MainPage__AppSection"]}>
-      <div className={classes["MainPage__AppSection--title"]}>
-        <h1>Introducing Sellix Business for iOS & Android</h1>
-        <p>
+    <div className="banerAndapp">
+      <div className="banerAndappDetail">
+        <span className="banerAndappDetail-span text-[40px] leading-[48px] mdMax:!text-[24px] mdMax:!leading-[32px]">
+          Introducing Sellix Business for iOS & Android
+        </span>
+        <p className="banerAndappDetail-p">
           Sellix is now available to be downloaded on mobile devices around the
           world on the App & Play Stores.
         </p>
         <div className="mt-10 md:mt-5 sm:mt-0">
-          <button className="btnBody btnBlack">
+          <button className="mb-5 btnBody btnBlack">
             <i class="mr-2 fa fa-apple"></i>
             App Store
           </button>
@@ -20,11 +22,13 @@ const AppSection = () => {
           </button>
         </div>
       </div>
-      <img
-        className={classes["MainPage__AppSection--img_digital"]}
-        src="https://cdn.sellix.io/static/landing/new/main/mobile-device.svg"
-        alt="mobile app"
-      />
+      <div className="w-[49%] mdMax:h-[20px] mdMax:w-full">
+        <img
+          className="banerAndappImg right-10 lgMax2:max-w-[400px] mdMax:!max-w-[300px] mdMax:static mdMax:mx-auto"
+          src="https://cdn.sellix.io/static/landing/new/main/mobile-device.svg"
+          alt="mobile app"
+        />
+      </div>
     </div>
   );
 };
--- src/pages/MainPageComponent/BanerSection.js
+++ src/pages/MainPageComponent/BanerSection.js
@@ -2,13 +2,13 @@ import star from "../../assets/star.png";
 
 const BanerSection = () => {
   return (
-    <div className="relative flex px-5 overflow-hidden py-36 mdMax:flex-col mdMax:text-center mdMax:pt-20">
-      <div className="w-[51%] mdMax:w-full mdMax:mb-14">
-        <span className="text-[88px] font-extrabold leading-[106px] whitespace-nowrap z-10 tracking-tighter lgMax:text-7xl mdMax:!text-[40px] mdMax:!leading-[48px] ">
+    <div className="banerAndapp">
+      <div className="banerAndappDetail">
+        <span className="banerAndappDetail-span text-[88px] whitespace-nowrap mdMax:text-center mdMax:!text-[40px] mdMax:!leading-[48px]">
           Digital selling
           <br /> with ease
         </span>
-        <p className="text-xl leading-[32px]  mt-5 mb-8 pr-10 flex-wrap mdMax:text-base mdMax:pr-0">
+        <p className="banerAndappDetail-p">
           Sell subscriptions, tokens, serial keys, digital downloads, video
           courses, softwares and licenses in a fast, easy and secure way.
         </p>
@@ -31,7 +31,7 @@ const BanerSection = () => {
       </div>
       <div className="w-[49%] mdMax:h-[450px]">
         <img
-          className="w-[975px] absolute top-20 -z-10 mdMax:top-auto smMax:max-w-[790px]"
+          className="banerAndappImg w-[975px] "
           src="https://cdn.sellix.io/static/landing/new/main/top-screen.png"
           alt="digital banner"
           loading="lazy"
